21bb7a0c7d31807a9d21d7b46f231b5164ba0fb4
Emirhan Pala <emrhnpla02@protonmail.com>
2022-05-01 14:49:38 +0300

    Add '@react-spring/parallax'

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,14 @@
 import type { NextPage } from "next";
+import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 const Home: NextPage = () => {
   return (
- <div>Home</div>
- );
+    <Parallax id="parallax" pages={5}>
+      <ParallaxLayer offset={0} speed={1.5}>
+        <div>Home</div>
+      </ParallaxLayer>
+    </Parallax>
+  );
 };
 
 export default Home;
