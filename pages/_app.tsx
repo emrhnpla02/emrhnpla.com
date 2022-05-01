a8bd66283173cbddc228a452cb2f7ed423e6f260
Emirhan Pala <emrhnpla02@protonmail.com>
2022-05-01 14:19:30 +0300

    Use layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import Layout from "../components/Layout";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider attribute="class">
+      <Layout>
         <Component {...pageProps} />
+      </Layout>
     </ThemeProvider>
   );
 }
