908a5442c533e2387d6b3950f2194d2949e07363
Emirhan Pala <emrhnpla02@protonmail.com>
2022-05-10 23:08:42 +0300

    Set up layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,26 @@ import { Parallax, ParallaxLayer } from "@react-spring/parallax";
 
 const Home: NextPage = () => {
   return (
-    <Parallax id="parallax" pages={5}>
-      <ParallaxLayer offset={0} speed={1.5}>
-        <div>Home</div>
-      </ParallaxLayer>
-    </Parallax>
+    <>
+      <Parallax
+        className="!top-12 !w-[calc(100%-7rem)]"
+        id="parallax"
+        pages={4}
+      >
+        <ParallaxLayer offset={0} speed={1}>
+          <p>About Me</p>
+        </ParallaxLayer>
+        <ParallaxLayer offset={1} speed={1}>
+          <p>Skills</p>
+        </ParallaxLayer>
+        <ParallaxLayer offset={2} speed={1}>
+          <p>My Projects</p>
+        </ParallaxLayer>
+        <ParallaxLayer offset={3} speed={1}>
+          <p>Contact</p>
+        </ParallaxLayer>
+      </Parallax>
+    </>
   );
 };
 
