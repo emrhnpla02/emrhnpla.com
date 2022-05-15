4653eedb3927b90699b755c0252dc0ddcb8d8bba
Emirhan Pala <emrhnpla02@protonmail.com>
2022-05-16 02:20:03 +0300

    Set dark theme as default theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => (
-  <Html lang="en" className="overflow-hidden w-full h-full m-0 p-0">
+  <Html lang="en" className="overflow-hidden w-full h-full m-0 p-0 dark">
     <Head />
     <body className="overflow-hidden w-full h-full m-0 p-0">
       <Main />
