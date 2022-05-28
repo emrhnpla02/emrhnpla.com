3b625d8e183e7b171b199d08c4817060ea65bbed
Emirhan Pala <emrhnpla02@protonmail.com>
2022-05-29 01:22:29 +0300

    Set light theme as default theme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 const Document = () => (
-  <Html lang="en" className="overflow-hidden w-full h-full m-0 p-0 dark">
+  <Html lang="en" className="overflow-hidden w-full h-full m-0 p-0">
     <Head />
     <body className="overflow-hidden w-full h-full m-0 p-0">
       <Main />
