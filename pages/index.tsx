eb690c2ebeed5d6fe9f7a481386bdbb423ec3610
Emirhan Pala <emrhnpla02@protonmail.com>
2022-05-30 16:32:47 +0300

    '&&' operator was causing 'false' class

--- pages/index.tsx
+++ pages/index.tsx
@@ -19,7 +19,7 @@ const Home: NextPage = () => {
     <>
       <Parallax
         className={`!top-12 !w-full xl:!w-[calc(100%-7rem)] ${
-          !showScrollbar && "!overflow-y-hidden"
+          !showScrollbar ? "!overflow-y-hidden" : ""
         } scrollbar-thin scrollbar-thumb-nord1 hover:scrollbar-thumb-nord0 scrollbar-track-nord10`}
         id="parallax"
         ref={parallaxRef}
