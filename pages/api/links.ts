1bf438f62739c8e019f557520b1c76fc5eba369a
Emirhan Pala <emrhnpla02@protonmail.com>
2022-06-16 23:45:18 +0300

    Update links

--- pages/api/links.ts
+++ pages/api/links.ts
@@ -15,24 +15,24 @@ export default function handler(
   res.status(200).json([
     {
       id: 1,
-      name: "Matrix",
-      username: "@emrhnpla02",
-      icon: "cib:matrix",
-      href: "https://matrix.to/#/@emrhnpla02:matrix.org",
-    },
-    {
-      id: 2,
       name: "Github",
       username: "emrhnpla02",
       icon: "logos:github-icon",
       href: "https://github.com/emrhnpla02",
     },
     {
-      id: 3,
-      name: "Mastodon",
+      id: 2,
+      name: "Twitter",
       username: "@emrhnpla02",
-      icon: "logos:mastodon-icon",
-      href: "https://mastodon.social/web/@emrhnpla02",
+      icon: "logos:twitter",
+      href: "https://twitter.com/emrhnpla02",
+    },
+    {
+      id: 3,
+      name: "Matrix",
+      username: "@emrhnpla02",
+      icon: "cib:matrix",
+      href: "https://matrix.to/#/@emrhnpla02:matrix.org",
     },
     {
       id: 4,
